429d159de9c828f54ce6e2a6ff9331460c607ba8
Eli-Tarrago <Eli.Tarrago@protonmail.com>
2019-10-21 18:59:17 -0400

    finished adding EditText view to allow input of a person\'s name

--- JustJava/app/src/main/java/com/example/justjava/MainActivity.java
+++ JustJava/app/src/main/java/com/example/justjava/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.justjava;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.TextView;
 import java.text.NumberFormat;
 
@@ -17,13 +19,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 
     /**
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-        displayMessage(createOrderSummery(numberOfCoffees, (numberOfCoffees * 5)));
+
+        CheckBox addWhipCream = (CheckBox) findViewById(R.id.checkbox_whipped_cream);
+        CheckBox addChocolate = (CheckBox) findViewById(R.id.checkbox_chocolate);
+        EditText nameOfPerson = (EditText) findViewById(R.id.enter_name_view);
+        displayMessage(createOrderSummery(nameOfPerson.getText().toString(),
+                numberOfCoffees,
+                (numberOfCoffees * 5),
+                addWhipCream.isChecked(),
+                addChocolate.isChecked()));
     }
 
     public void increment(View view) {
@@ -37,7 +48,6 @@ public class MainActivity extends AppCompatActivity {
         displayQuantity(numberOfCoffees);
         displayPrice(numberOfCoffees * 5);
     }
-    
 
     /**
      * This method displays the given quantity value on the screen.
@@ -62,17 +72,21 @@ public class MainActivity extends AppCompatActivity {
         TextView orderSummeryTextView = (TextView) findViewById(R.id.orderSummeryTextView);
         orderSummeryTextView.setText(message);
     }
-
     /**
      * Creates the order Summery to be displayed to user.
+     * @param personName String value of a person's name who is ordering
      * @param quantity number of items
      * @param priceOfOrder price of total items.
+     * @param addWhipCream did user add whip cream to their order?
+     * @param addChocolate did user add chocolate to their order?
      * @return string message thanking user.
      */
-    private String createOrderSummery(int quantity, int priceOfOrder) {
-        return("Name: Kaptain Kunal\n"
+    private String createOrderSummery(String personName, int quantity, int priceOfOrder, boolean addWhipCream, boolean addChocolate) {
+        return("Name: " + personName + "\n"
                 + "Quantity: " + quantity + "\n"
                 + "Total: " + priceOfOrder + "\n"
+                + "Add Whipped Cream? " + addWhipCream + "\n"
+                + "Add Chocolate? " + addChocolate + "\n"
                 + "Thank You!");
     }
 }
