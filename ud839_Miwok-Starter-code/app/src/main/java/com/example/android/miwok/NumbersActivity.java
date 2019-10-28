98a1b0b6b0f7e3fe6bfe51e02d622dbe50e23d76
Eli-Tarrago <Eli.Tarrago@protonmail.com>
2019-10-28 14:56:59 -0400

    fixed java install on local machine, upgraded local java version from 13.0.0 to 13.0.1, added new string value to contain arraylist of english words

--- ud839_Miwok-Starter-code/app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ ud839_Miwok-Starter-code/app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -3,11 +3,19 @@ package com.example.android.miwok;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class NumbersActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_numbers);
+
+        // Adding English words
+        String words [] = new String [] {"one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten"};
+        ArrayList<String> wordsList = new ArrayList<>(Arrays.asList("one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten"));
     }
 }
