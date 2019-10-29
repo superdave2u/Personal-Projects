0c29f87ff985955162f2f8c1cfcf1b6977a7c59b
Eli-Tarrago <Eli.Tarrago@protonmail.com>
2019-10-28 22:58:59 -0400

    adding new ArrayAdapter, custom array class, custom text view

--- ud839_Miwok-Starter-code/app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ ud839_Miwok-Starter-code/app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -1,9 +1,11 @@
 package com.example.android.miwok;
 
 import androidx.appcompat.app.AppCompatActivity;
-import android.os.Bundle;
 
-import java.lang.reflect.Array;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 
@@ -14,8 +16,25 @@ public class NumbersActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_numbers);
 
-        // Adding English words
-        String words [] = new String [] {"one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten"};
-        ArrayList<String> wordsList = new ArrayList<>(Arrays.asList("one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten"));
+
+        ArrayList<Word> wordsList = new ArrayList<Word>();
+        wordsList.add(new Word("one", "lutti"));
+        wordsList.add(new Word("two", "otiiko"));
+        wordsList.add(new Word("three", "tolookosu"));
+        wordsList.add(new Word("four", "oyyisa"));
+        wordsList.add(new Word("five", "massokka"));
+        wordsList.add(new Word("six", "temmokka"));
+        wordsList.add(new Word("seven", "kenekaku"));
+        wordsList.add(new Word("eight", "kawinta"));
+        wordsList.add(new Word("nine", "wo’e"));
+        wordsList.add(new Word("ten", "na’aacha"));
+
+
+        WordAdapter itemsAdapter = new WordAdapter(this, wordsList);
+        ListView listView = (ListView) findViewById(R.id.list);
+        listView.setAdapter(itemsAdapter);
+
+
     }
+
 }
