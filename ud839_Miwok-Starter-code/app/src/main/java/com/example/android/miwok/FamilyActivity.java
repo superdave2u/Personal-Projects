35e7ceeff194c5aa2816ead3bbf32917b8dd40f1
Eli-Tarrago <Eli.Tarrago@protonmail.com>
2019-10-29 14:22:24 -0400

    okay, that last commit wasn't true, that was the actually the end of module 3, lesson 2. But really this is the last bit. Removed un-used assets

--- ud839_Miwok-Starter-code/app/src/main/java/com/example/android/miwok/FamilyActivity.java
+++ ud839_Miwok-Starter-code/app/src/main/java/com/example/android/miwok/FamilyActivity.java
@@ -2,12 +2,34 @@ package com.example.android.miwok;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class FamilyActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_family);
+        setContentView(R.layout.word_list);
+
+
+        ArrayList<Word> wordsList = new ArrayList<Word>();
+        wordsList.add(new Word("father", "әpә"));
+        wordsList.add(new Word("mother", "әṭa"));
+        wordsList.add(new Word("son", "angsi"));
+        wordsList.add(new Word("daughter", "tune"));
+        wordsList.add(new Word("older brother", "taachi"));
+        wordsList.add(new Word("younger brother", "chalitti"));
+        wordsList.add(new Word("older sister", "teṭe"));
+        wordsList.add(new Word("younger sister", "kolliti"));
+        wordsList.add(new Word("grandmother", "ama"));
+        wordsList.add(new Word("grandfather", "paapa"));
+
+
+        WordAdapter itemsAdapter = new WordAdapter(this, wordsList);
+        ListView listView = (ListView) findViewById(R.id.list);
+        listView.setAdapter(itemsAdapter);
+
+
     }
 }
--- ud839_Miwok-Starter-code/app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ ud839_Miwok-Starter-code/app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -3,18 +3,16 @@ package com.example.android.miwok;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 
 public class NumbersActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_numbers);
+        setContentView(R.layout.word_list);
 
 
         ArrayList<Word> wordsList = new ArrayList<Word>();
