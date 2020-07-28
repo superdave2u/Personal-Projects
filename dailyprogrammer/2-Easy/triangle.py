d10fb8417e40e00a09bff70b9dbbe0f0658d9f08
Eli Tarrago <eli.tarrago@protonmail.com>
2020-07-27 22:48:10 -0400

    Bug fix, fix the triangle drawing

--- dailyprogrammer/2-Easy/triangle.py
+++ dailyprogrammer/2-Easy/triangle.py
@@ -9,12 +9,12 @@ total = trig.pythagorean(opposite=2.8, hypotenuse=hypotenuse['hypotenuse'])
 print(total)
 board = turtle.Turtle()
 
-board.forward(total['hypotenuse'])  # draw base
+board.forward(total['adjacent'] * 50)  # draw base
 
 board.left(90)
-board.forward(total['opposite'])
+board.forward(total['opposite'] * 50)
 
-board.left(135)
-board.forward(total['adjacent'])
+board.left(125)
+board.forward(total['hypotenuse'] * 50)
 
 turtle.done()
